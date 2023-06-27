2c2ae65262205cf2e5c2999dce2e9653a62d688a
Nafiu Adegbite <youngguru23@gmail.com>
2023-06-27 15:36:12 +0100

    fix: redis on vercel

--- services/redis.js
+++ services/redis.js
@@ -1,5 +1,11 @@
 const redis = require("ioredis");
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  password: "3qfRzzQxNUscJZ8yidc9ptVyKhIwwFhn",
+  socket: {
+    host: "redis-10820.c226.eu-west-1-3.ec2.cloud.redislabs.com",
+    port: 10820,
+  },
+});
 
 redisClient.on("connect", () => {
   console.log("connected to redis successfully!");
